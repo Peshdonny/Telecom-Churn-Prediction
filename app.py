f8ca049a2c48738987e67a388dd110729f3a876a
Peshdonny <114675579+Peshdonny@users.noreply.github.com>
2026-04-26 14:28:38 +0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Telecom Churn App")
-
-st.title("📡 Telecom Customer Churn Prediction")
-
-st.write("Welcome to the telecom churn prediction demo app.")
-
-st.write("Next step: we shall connect the machine learning model.")
